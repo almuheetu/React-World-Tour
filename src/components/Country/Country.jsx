093ec730170ef1ba54332365ad6c391d8594130b
Almuheetu Shihab <almuheetu@gmail.com>
2024-03-24 13:40:11 +0600

    onClick a eventt lesaner call

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import './Country.css';
 
 
 const Country = ({country}) => {
   const {name, flags, population, area, cca3} = country;
+
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(true);
+  }
     return (
         <div className='Country'>
-            <h3>Name: {name?.common}</h3>
+            <h3>{name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>area: {area} </p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited}>Visited</button>
+            {visited && 'I have visited Country.'}
         </div>
     );
 };
