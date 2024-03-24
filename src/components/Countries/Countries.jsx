8d9688add16961bec04fe8c5981f9fd43d13a59b
Almuheetu Shihab <almuheetu@gmail.com>
2024-03-24 13:30:48 +0600

    dainamic data passing and ui shows

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 
 
@@ -13,7 +14,11 @@ const Countries = () => {
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
-           
+           <div className="country-container">
+           {
+                countries.map(country =><Country key={country.cca3} country={country}></Country>)
+            }
+           </div>
         
         </div>
     );
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,15 @@
 import './Country.css';
-// import React from 'react';
+
 
 const Country = ({country}) => {
-  const {name, flags} = country;
+  const {name, flags, population, area, cca3} = country;
     return (
         <div className='Country'>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>area: {area} </p>
+            <p><small>Code: {cca3}</small></p>
         </div>
     );
 };
