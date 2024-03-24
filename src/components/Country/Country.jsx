d2d5ba2cecdce2831a2243e2bd37ce57f125b19c
Almuheetu Shihab <almuheetu@gmail.com>
2024-03-25 03:23:33 +0600

    dainamic function pass

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,20 +2,27 @@ import { useState } from 'react';
 import './Country.css';
 
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
   const {name, flags, population, area, cca3} = country;
 
   const [visited, setVisited] = useState(false);
   const handleVisited = () => {
     setVisited(!visited);
   }
+
+  console.log(handleVisitedCountry);
+
+  
     return (
-        <div className= {`Country ${visited && 'visited'}`} >
-            <h3>{name?.common}</h3>
+        <div className= {`Country ${visited ? 'visited' : 'non-visited'}`} >
+            <h3 style={{color: visited ? 'purple' : 'black'}}>{name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>area: {area} </p>
             <p><small>Code: {cca3}</small></p>
+           
+            <button>Mark visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited Country.' : 'I want to visit this country'}
         </div>
